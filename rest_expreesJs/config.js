d6fec3bcce66bf696284947ea3e72b01a1ba856f
jeevan.b <jeevan@smartenspaces.com>
2021-03-10 19:28:03 +0530

    node api with express js

--- rest_expreesJs/config.js
+++ rest_expreesJs/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGO_URI:"localhost:3000/mongodb",
+  MONGO_URI:"mongodb://jeevan:te2yQtm4XVXMYcw1@cluster0-shard-00-00.ahi0n.mongodb.net:27017,cluster0-shard-00-01.ahi0n.mongodb.net:27017,cluster0-shard-00-02.ahi0n.mongodb.net:27017/jeevanDb?ssl=true&replicaSet=atlas-71z7py-shard-0&authSource=admin&retryWrites=true&w=majority",
 };
