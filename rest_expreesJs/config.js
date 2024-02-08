ceb7f48b8bd5d576fa7461adfe92580e1bac9308
jeevan520 <bjeevan.520@gmail.com>
2024-02-08 14:02:05 +0530

    Update config.js

--- rest_expreesJs/config.js
+++ rest_expreesJs/config.js
@@ -2,4 +2,3 @@ module.exports = {
   MONGO_URI:"mongodb://localhost/jeevanDb",
 };
 
-//MONGO_URI=mongodb://jeevan:te2yQtm4XVXMYcw1@cluster0-shard-00-00.ahi0n.mongodb.net:27017,cluster0-shard-00-01.ahi0n.mongodb.net:27017,cluster0-shard-00-02.ahi0n.mongodb.net:27017/jeevanDb?ssl=true&replicaSet=atlas-71z7py-shard-0&authSource=admin&retryWrites=true&w=majority
